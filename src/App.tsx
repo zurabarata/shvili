3a9230e7878d059f3d6c614abb4e0573c21e9574
Zurab <zurab@candis.io>
2022-09-22 17:51:07 +0200

    redirect to main by default

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ const App: React.FC = () => (
             <Tab3 />
           </Route>
           <Route exact path="/">
-            <Redirect to="/tab1" />
+            <Redirect to="/main" />
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
