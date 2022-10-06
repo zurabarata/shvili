0b4e796b45919bfae4c25ded757d12231360b8f6
Zurab <zurab@candis.io>
2022-10-06 17:22:23 +0200

    add loading state

--- src/pages/Quotes.tsx
+++ src/pages/Quotes.tsx
@@ -1,7 +1,7 @@
 import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
-import React from "react";
+import React, {useState} from "react";
 import {fetchQuote} from "./../utils/fetch-quote";
 import {refresh} from "ionicons/icons";
 
@@ -9,12 +9,15 @@ export const Quotes: React.FC = () => {
 
     const [quote, setQuote] = React.useState('');
     const [author, setAuthor] = React.useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     React.useEffect(() => {
+        setIsLoading(true);
         fetchQuote().then((data) => {
             setQuote(data.content);
             setAuthor(data.author);
         });
+        setIsLoading(false);
     }
     , []);
 
@@ -31,10 +34,10 @@ export const Quotes: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Quotes</IonTitle>
+            {/*<IonTitle size="large">Quotes</IonTitle>*/}
           </IonToolbar>
         </IonHeader>
-          <ExploreContainer name={quoteWithAuthor} icon={refresh} />
+          <ExploreContainer name={isLoading ? 'fetching...' : quoteWithAuthor} icon={refresh} />
       </IonContent>
     </IonPage>
   );
