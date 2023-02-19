67f9669c168bd35f5d5185d6afedabc8582e2260
Zurab <zurab@candis.io>
2023-02-19 23:52:19 +0100

    add card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,33 @@
+import React, {FunctionComponent} from 'react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import zurabPhoto from '../Zurab Baratashvili.jpeg';
+
+interface CardProps {
+    title: string;
+    subtitle: string;
+    content: string;
+    image: string;
+    cardWidth?: string;
+}
+
+
+export const Card: FunctionComponent<CardProps> = ({title, subtitle, content, image, cardWidth}) => {
+
+    return (
+        <IonCard style={{
+            maxWidth: cardWidth ?? '300px',
+            margin: 'auto',
+marginTop: '50px'
+        }}>
+            <img alt="Zurab Baratashvili" src={image}/>
+            <IonCardHeader>
+                <IonCardTitle>{title}</IonCardTitle>
+                <IonCardSubtitle>{subtitle}</IonCardSubtitle>
+            </IonCardHeader>
+
+            <IonCardContent>
+                {content}
+            </IonCardContent>
+        </IonCard>
+    );
+}
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,7 +2,8 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import React from "react";
-import zurab from '../Zurab-Baratashvili.svg';
+import zurabImage from '../Zurab Baratashvili.jpeg';
+import {Card} from "../components/Card";
 
 const aboutMe = "hi i'm zurab: \n " +
     "a software dev and\n" +
@@ -24,7 +25,9 @@ export const Main: React.FC = () => {
             {/*<IonTitle size="large">Tab 1</IonTitle>*/}
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name={aboutMe} icon={zurab} />
+        <ExploreContainer name={aboutMe}/>
+         {/* // todo uncomeent this card and remove the above*/}
+          {/*<Card title="👋🏼" subtitle="me" content={aboutMe} image={zurabImage}/>*/}
       </IonContent>
     </IonPage>
   );
